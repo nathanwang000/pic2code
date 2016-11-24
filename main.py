46fddda4e07b6a1f6fb94ae3ef0fd0984da2f0ee
Jiaxuan Wang <nathanwang000@gmail.com>
2016-11-23 22:46:56 -0500

    able to save image data

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request
+import base64
+import re
+from PIL import Image
+from io import BytesIO
 
 app = Flask(__name__, static_folder='static', static_url_path='/static')
 
-@app.route('/')
+@app.route('/', methods = ['POST', 'GET'])
 def index():
+    if request.method == 'POST':
+        image_data = base64.b64decode(re.sub('^data:image/.+;base64,', '', request.form['img']))
+        im = Image.open(BytesIO(image_data))
+        im.show()
     return render_template('index.html')
 
 if __name__ == '__main__':
--- static/index.js
+++ static/index.js
@@ -6,22 +6,25 @@ $(document).ready(function(){
     if(navigator.mediaDevices &&
        navigator.mediaDevices.getUserMedia) {
 	// Not adding `{ audio: true }` since we only want video now
-	navigator.mediaDevices.getUserMedia({ video: true }).\
-	then(function(stream) {
+	navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
 	    video.src = window.URL.createObjectURL(stream);
 	    video.play();
 	});
     }
     
-    
     // Elements for taking the snapshot
     var canvas = document.getElementById('canvas');
     var context = canvas.getContext('2d');
     var video = document.getElementById('video');
-    
+
     // Trigger photo take
-    document.getElementById("snap").\
-    addEventListener("click", function() {
+    $("#snap").click(function() {
 	context.drawImage(video, 0, 0, 640, 480);
+	var dataURL = canvas.toDataURL();
+	$.ajax({
+	    type: 'POST',
+	    data: {img: dataURL} // b64 image
+	})
+
     });
 });
